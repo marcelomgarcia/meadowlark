3711545cbf4314e7225f5d56addbf1e949c62561
Marcelo Garcia <marcelomgarcia@gmail.com>
2021-09-25 00:18:26 +0300

    Using handlebars routes

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const exressHandlebars = require('express-handlebars')
+const expressHandlebars = require('express-handlebars')
 
 const app = express()
 
@@ -12,32 +12,20 @@ app.engine('handlebars', expressHandlebars({
 
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Meadowlark Travel')
-})
+app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send('About Meadowlark Travel')
-})
+app.get('/about', (req, res) => res.render('about'))
 
-// custom 404 page
+// Custom 404 page
 app.use((req, res) => {
-    res.type('text/plain')
-    res.status(500)
-    res.send('404 - Not Found')
+    res.status(404)
+    res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.type('text/plain')
-  res.status(500)
-  res.send('500 - Server Error')
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
 })
 
-app.listen(port, () => console.log(
-    `Express started on http://localhost:${port}; ` + 
-    `press Ctrl-C to terminate.`
-))
